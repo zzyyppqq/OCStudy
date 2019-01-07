149466ee9f06f3809ca6c1202d088c730b7a3403
zhangyipeng <zhangyipeng@yongche.com>
2019-01-07 15:56:26 +0800

    sh6

--- create_story_branch.sh
+++ create_story_branch.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-echo 参数列表$@
-params=$@
-array=(${params// / })
-i=0
-if [ $# -eq 0 ] || [ $# -eq 1 ];then
-    echo 请输入基于哪个远程分支创建story分支[./create_story_branch.sh origin/developer story_1 story_2 ...]
-else
-    for var in ${array[@]}
-    do
-       if [ $i -gt 0 ];then
-           echo 创建分支：git checkout -b $var ${array[0]}
-           git checkout -b $var ${array[0]}
-           git push -u origin $var
-           echo 创建分支完成：$var
 
-        fi
-        i=`expr $i + 1`
-    done
+if [ $# -eq 0 ];then
+    echo 请输入基于哪个远程分支创建story分支[./create_story_branch.sh origin/developer]
+else
+    while read line
+    do
+        echo 创建分支：$line $1
+        git checkout -b $line $1
+        git push -u origin $line
+        echo 创建分支完成：$line
+    done < branch.txt
     echo 批量创建分支完成
     git branch -a > temp.txt
     echo 显示所有本地分支、远程分支
