2eabda98a78c74381d1d63b3a789259ccb632868
zhangyipeng <zhangyipeng@yongche.com>
2019-01-07 15:08:36 +0800

    sh4

--- create_story_branch.sh
+++ create_story_branch.sh
@@ -3,15 +3,13 @@ echo 参数列表$@
 params=$@
 array=(${params// / })
 i=0
-zero=0
 if [ $# -eq 0 ] || [ $# -eq 1 ];then
     echo 请输入基于哪个远程分支创建story分支[./create_story_branch.sh origin/developer story_1 story_2 ...]
 else
     for var in ${array[@]}
     do
-       if [ $i -gt $zero ];then
-           git checkout master
-           echo 创建分支：$var ${array[0]}
+       if [ $i -gt 0 ];then
+           echo 创建分支：git checkout -b $var ${array[0]}
            git checkout -b $var ${array[0]}
            git push -u origin $var
            echo 创建分支完成：$var
